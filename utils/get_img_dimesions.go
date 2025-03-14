7935d067a1c03e28e7fa9d226f149440508cfe2e
Yonatan Rojas <yonatan.rojas@payclip.com>
2025-03-14 11:16:24 -0600

    feat: created GetImageDimentsions function to get width and height of assets

---
+++ utils/get_img_dimesions.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"image"
+	"os"
+)
+
+func GetImageDimensions(path string) (int, int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer file.Close()
+
+	config, _, err := image.DecodeConfig(file)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return config.Width, config.Height, nil
+}
