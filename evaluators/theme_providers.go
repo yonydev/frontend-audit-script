76599314944ba9295d4e082c10554493a2e35936
Yonatan Rojas <yonatan.rojas@payclip.com>
2025-03-27 11:44:39 -0600

    feat: printed theme provider components and score logic

--- evaluators/theme_providers.go
+++ evaluators/theme_providers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 	"sync"
 
 	c "github.com/yonydev/frontend-audit-script/colorize"
@@ -12,11 +13,19 @@ import (
 
 func EvalThemeProviders(paths []string) (Evaluation, error) {
 	var filesUsingThemeProvider []string
+	var themeProvidersNames []string
 	var messages []string
+
+	uniqueProviders := make(map[string]struct{})
 	tagPattern := regexp.MustCompile(`<(ThemeProvider|ThemeProviders|MuiThemeProvider|UIThemeProvider)\b[^>]*?>`)
 
 	evalName := ">>> Theme Provider Check\n"
 	evalDesc := "Checking for theme provider components in files...\n"
+	initialScore := 100
+	minScore := 40
+	maxScore := 100
+	weight := 3
+	penaltyPoints := 0
 
 	type result struct {
 		path string
@@ -48,8 +57,10 @@ func EvalThemeProviders(paths []string) (Evaluation, error) {
 
 			for scanner.Scan() {
 				line := scanner.Text()
-				if tagPattern.MatchString(line) {
+				matches := tagPattern.FindStringSubmatch(line)
+				if len(matches) > 1 {
 					filesUsingThemeProvider = append(filesUsingThemeProvider, path)
+					uniqueProviders[matches[1]] = struct{}{}
 					break
 				}
 			}
@@ -74,7 +85,14 @@ func EvalThemeProviders(paths []string) (Evaluation, error) {
 		}
 	}
 
-	if len(filesUsingThemeProvider) == 0 {
+	for themeName := range uniqueProviders {
+		themeProvidersNames = append(themeProvidersNames, themeName)
+	}
+
+	numProviders := len(uniqueProviders)
+	numFiles := len(filesUsingThemeProvider)
+
+	if numProviders == 0 {
 		messages = append(
 			messages,
 			"\nNo ThemeProvider found in any of the .js(x) or .ts(x) files. Consider using a theme provider for consistent theme usage.",
@@ -82,23 +100,31 @@ func EvalThemeProviders(paths []string) (Evaluation, error) {
 	} else {
 		messages = append(
 			messages,
-			fmt.Sprintf(
-				"\nTotal of %s files found theme provider components in the following files:",
-				c.InfoFgBold(len(filesUsingThemeProvider)),
-			),
+			fmt.Sprintf("Using: %s", c.InfoFgBold(strings.Join(themeProvidersNames, ", "))),
+			fmt.Sprintf("\nTotal of %s files found with theme provider components:", c.InfoFgBold(numFiles)),
 		)
 		for _, file := range filesUsingThemeProvider {
 			messages = append(messages, fmt.Sprintf("file: %s", c.WarningFg(file)))
 		}
 	}
 
+	// Scoring logic
+	if numProviders > 1 {
+		penaltyPoints += 30 // Multiple theme providers -> major penalty
+	}
+	if numFiles > 1 {
+		penaltyPoints += (numFiles - 1) * 10 // Each extra file adds a penalty
+	}
+
+	score := max(initialScore-penaltyPoints, minScore)
+
 	return NewEvaluation(
 			evalName,
 			evalDesc,
-			0,
-			0,
-			0,
-			0,
+			score,
+			maxScore,
+			minScore,
+			weight,
 			messages,
 		),
 		nil
