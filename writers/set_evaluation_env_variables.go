f6ced19f2874fb13f81611e9545e425a882c1ffa
Yonatan Rojas <yonatan.rojas@payclip.com>
2025-03-27 18:08:46 -0600

    feat: created setter for env variables

---
+++ writers/set_evaluation_env_variables.go
@@ -0,0 +1,27 @@
+package writers
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/sethvargo/go-githubactions"
+	"github.com/yonydev/frontend-audit-script/models"
+)
+
+func SetEvaluationEnvVariables(evaluation models.Evaluation, envVars map[string]string) {
+  evalMap := map[string]string{
+		"Name":     evaluation.Name,
+		"Score":    strconv.Itoa(evaluation.Score),
+		"MaxScore": strconv.Itoa(evaluation.MaxScore),
+		"MinScore": strconv.Itoa(evaluation.MinScore),
+		"Weight":   strconv.Itoa(evaluation.Weight),
+	}
+
+	for envKey, field := range envVars {
+		if value, exists := evalMap[field]; exists {
+			githubactions.SetEnv(envKey, value)
+			fmt.Printf("✅ Set %s=%s\n", envKey, value) // Debug output
+		}
+	}
+}
+
