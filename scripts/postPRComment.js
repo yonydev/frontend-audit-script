ebd2ce2264c8a9f5ad649980d410a7827d114003
Yonatan Rojas <yonatan.rojas@payclip.com>
2025-03-31 16:04:55 -0600

    updated action.yml

--- scripts/postPRComment.js
+++ scripts/postPRComment.js
@@ -1,1 +1,32 @@
-console.log("-------running postPRComment.js------");
+module.exports = async function postPrComment({ github, context, finalScore }) {
+  const message = `
+  ### PR Comment
+  ${finalScore}
+`;
+
+  const { data: comments } = await github.rest.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+  });
+
+  const existingComment = comments.find((comment) =>
+    comment.body.includes(uniqueIdentifier),
+  );
+
+  if (existingComment) {
+    await github.rest.issues.updateComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      comment_id: existingComment.id,
+      body: message,
+    });
+  } else {
+    await github.rest.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.issue.number,
+      body: message,
+    });
+  }
+};
